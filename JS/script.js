cd603c0892e6019b7acd6630961d5661411a0d23
Kimateru <dancikpalicik@yahoo.comm>
2022-09-06 02:15:00 +0300

    1 phase of movie application

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,30 @@
 
 
 
+let numberOfFilms=+prompt('Сколько фильмов вы уже посмотрели ?',"");
 
 
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{
+
+    },
+    actors:{
+
+    },
+    genres:[],
+    privat:false
+};
+
+let lastMovie1=prompt('Один из последних просмотренных фильмов ?',"");
+let movieRatingUser1=prompt('На сколько его оцените ?',"");
+let lastMovie2=prompt('Один из последних просмотренных фильмов ?',"");
+let movieRatingUser2=prompt('На сколько его оцените ?',"");
+
+personalMovieDB.movies[lastMovie1]=movieRatingUser1;
+personalMovieDB.movies[lastMovie2]=movieRatingUser2;
+
+console.log(personalMovieDB);
 
 
 
